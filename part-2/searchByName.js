bf49a17d6eb4f65b3f3369819aef396fa79776c6
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-19 16:08:29 -0700

    Add if statement finding objects with 'rep_name' starting with 'name' input

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -2,8 +2,10 @@ var clients = require('./clients.json')
 
 function searchByName(name){
   for(i = 0; i < clients.length; i++) {
-    console.log(clients[i].rep_name)
+    if (clients[i].rep_name.startsWith(name)){
+      console.log(clients[i].rep_name)
+    }
   }
 }
-
-console.log(searchByName())
+searchByName("Xy")
+// console.log(searchByName())
