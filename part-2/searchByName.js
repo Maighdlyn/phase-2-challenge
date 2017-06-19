5b018e7bd631c01e8cda0a25e060f1583b3decc9
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-19 16:41:03 -0700

    Change from function format to script that can run with node

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -1,9 +1,7 @@
 var clients = require('./clients.json')
 
-function searchByName(name){
-  for(i = 0; i < clients.length; i++) {
-    if (clients[i].rep_name.startsWith(name)){
-      console.log("id:", clients[i].id, "name:", clients[i].rep_name)
-    }
+for(i = 0; i < clients.length; i++) {
+  if (clients[i].rep_name.startsWith(process.argv[2])){
+    console.log("id:", clients[i].id, "name:", clients[i].rep_name)
   }
 }
