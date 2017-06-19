60451e7af80c231fde820fc3c276d110256cacb9
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-19 16:46:55 -0700

    Change cases so upper or lowercase searches yield same results

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -1,7 +1,7 @@
 var clients = require('./clients.json')
 
 for(i = 0; i < clients.length; i++) {
-  if (clients[i].rep_name.startsWith(process.argv[2])){
+  if (clients[i].rep_name.toUpperCase().startsWith(process.argv[2].toUpperCase())){
     console.log("id:", clients[i].id, "name:", clients[i].rep_name)
   }
 }
