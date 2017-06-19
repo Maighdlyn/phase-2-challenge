9031070eee6b83e0b5c3994b521afd79f90b2be9
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-19 16:53:33 -0700

    Add searchByState script completing exercise 2 of part 2

---
+++ part-2/searchByState.js
@@ -0,0 +1,13 @@
+var clients = require('./clients.json')
+
+for(i = 0; i < clients.length; i++) {
+  if (clients[i].state.toUpperCase() === process.argv[2].toUpperCase()){
+    console.log(
+      "id:", clients[i].id,
+      "name:", clients[i].rep_name,
+      "company:", clients[i].company,
+      "city:", clients[i].city,
+      "state:", clients[i].state
+    )
+  }
+}
