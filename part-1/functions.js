3cb40b5511769e7cb91b0734db9de7651fc0f8af
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-20 11:15:48 -0700

    Put all functions inside object for exporting

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,51 +1,27 @@
-// Show a weekday
-
-// Get a snippet from text
-function snippet (string, maxLength) {
-  if (string.length > maxLength) {
-    var shortenedString = string.slice(0,maxLength) + "..."
-    return shortenedString
-  }
-  else {
-    return string
-  }
-}
-
-// Number of properties
-function numProps(obj) {
-  return Object.keys(obj).length
-}
-
-// Filter between
-function filterBetween (array, min, max) {
-  var newArray = []
-  for(i=0; i<array.length; i++){
-    if (array[i] >= min && array[i] <= max)
-    newArray.push(array[i])
-  }
-  return newArray
-}
-
-// module.exports = function (){
-//   return 'hellos'
-// }
-
-function hello(){
-  return "hello"
-}
-
-
 module.exports = {
-  hello: hello(),
-  snippet: function (string, maxLength) {
-    if (string.length > maxLength) {
-      var shortenedString = string.slice(0,maxLength) + "..."
-      return shortenedString
+  snippet:
+    function (string, maxLength) {
+      if (string.length > maxLength) {
+        var shortenedString = string.slice(0,maxLength) + "..."
+        return shortenedString
+      }
+      else {
+        return string
+      }
+    },
+
+  numProps:
+    function (obj) {
+      return Object.keys(obj).length
+    },
+    
+  filterBetween:
+    function (array, min, max) {
+      var newArray = []
+      for(i=0; i<array.length; i++){
+        if (array[i] >= min && array[i] <= max)
+        newArray.push(array[i])
+      }
+      return newArray
     }
-    else {
-      return string
-    }
-  },
-  numProps: numProps({}),
-  filterBetween: filterBetween([])
 }
