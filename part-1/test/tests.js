d53abc5f52e62b798b8c38785738eff6f22f9c51
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-20 12:57:30 -0700

    Add 'Number of properties' tests and begin working on 'Filter between' tests

--- part-1/test/tests.js
+++ part-1/test/tests.js
@@ -3,27 +3,39 @@ const snippet = require('../functions.js').snippet
 const numProps = require('../functions.js').numProps
 const filterBetween = require('../functions.js').filterBetween
 
-
-describe("Get a snippet from text", (done) => {
-  it('Shortens string correctly', () => {
-    let result = snippet("For the following exercises,", 10)
-    expect(result).to.equal("For the fo...")
+describe("Phase 2 Challenge: Part 1", () => {
+  describe("Get a snippet from text", (done) => {
+    it('Shortens string correctly', () => {
+      expect(snippet("For the following exercises,", 10)).to.equal("For the fo...")
+    })
+    it("Displays whole string when shorter than maxLength", () =>{
+      expect(snippet("Hello, world!", 20)).to.equal("Hello, world!")
+    })
+    // it("Throws error when string input is not a string", () => {
+    //   expect(snippet(5, 10)).to.throw()
+    // })
+    // it("Throws error when maxLength is not a number", () => {
+    //     expect(snippet("I'm a string", "I'm also a string")).to.throw()
+    // })
   })
-  it("Displays whole string when shorter than maxLength", () =>{
-    let result = snippet("Hello, world!", 20)
-    expect(result).to.equal("Hello, world!")
+  describe("Number of properties", () => {
+    it("Returns number of properties for provided 'friends' example", () => {
+      let friend = {
+        name: 'Dominique',
+        age: 30,
+        phone: '555-555-5555'
+      }
+      expect(numProps(friend)).to.equal(3)
+    })
+    it("Returns 0 properties for an empty object", () => {
+      expect(numProps({})).to.equal(0)
+    })
+    //  A test using unexpected/invalid inputs for the numProps() function is written in tests.js.
   })
-  // it("Throws errors with unexpected input", () => {
-  //   let result = snippet("I'm a string", "I'm also a string")
-  //
-  // })
+  describe("Filter between", () => {
+    let arr = [1, 2, 3, 4, 5, 6, 7]
+    it ("filters array", () => {
+      expect(filterBetween(arr, 3, 6)).to.equal([3, 4, 5, 6])
+    })
+  })
 })
-
-
-//   it("Displays whole string when appropriate", () => {
-//     expect(snippet("Hello, world!", 20)).to.equal("Hello, world!")
-//   })
-// })
-
-// ("For the following exercises,", 10) // => "For the fo…"
-// })
