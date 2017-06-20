4373710af309c49922ec4e0d4c2d120f9d2896f7
maighdlyn <emmamaighdlyn@gmail.com>
2017-06-20 11:30:26 -0700

    Add snippet test that displays the full word

--- part-1/test/tests.js
+++ part-1/test/tests.js
@@ -1,5 +1,4 @@
 const expect = require('chai').expect
-const assert = require('chai').assert
 const snippet = require('../functions.js').snippet
 const numProps = require('../functions.js').numProps
 const filterBetween = require('../functions.js').filterBetween
@@ -8,14 +7,19 @@ const filterBetween = require('../functions.js').filterBetween
 describe("Get a snippet from text", (done) => {
   it('Shortens string correctly', () => {
     let result = snippet("For the following exercises,", 10)
-    assert.equal(result, "For the fo...")
+    expect(result).to.equal("For the fo...")
   })
+  it("Displays whole string when shorter than maxLength", () =>{
+    let result = snippet("Hello, world!", 20)
+    expect(result).to.equal("Hello, world!")
+  })
+  // it("Throws errors with unexpected input", () => {
+  //   let result = snippet("I'm a string", "I'm also a string")
+  //
+  // })
 })
 
-// describe('Get a snippet from text', (done) => {
-//   it("Shortens string correctly", () => {
-//     expect(snippet("For the following exercises,", 10)).to.equal("For the fo…")
-//   })
+
 //   it("Displays whole string when appropriate", () => {
 //     expect(snippet("Hello, world!", 20)).to.equal("Hello, world!")
 //   })
